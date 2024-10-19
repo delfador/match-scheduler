793305446fc856e9eefaff7df1eb1c3c129bbb2e
Ruud Brekelmans <ruud.brekelmans@gmail.com>
2024-10-19 21:37:44 +0200

    Make simple main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 package org.ruud
 
-import org.ruud.schedule.Round
+import org.ruud.schedule.Problem
+import org.ruud.solver.RandomInitializer
 
 fun main() {
-    val round = Round((0..4).toList(), 4)
+    val problem = Problem(10, 10, 4)
+    val schedule = RandomInitializer(problem).create()
+    println(schedule)
 }
