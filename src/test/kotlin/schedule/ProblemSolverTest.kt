9d85f9880b7a6e4b6fa16a46a3b4fd920523fc8c
Ruud Brekelmans <ruud.brekelmans@gmail.com>
2024-10-26 15:57:59 +0200

    Test ProblemSolver

---
+++ src/test/kotlin/schedule/ProblemSolverTest.kt
@@ -0,0 +1,25 @@
+package schedule
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.ruud.schedule.Problem
+import org.ruud.schedule.ProblemSolver
+
+class ProblemSolverTest {
+    @Test
+    fun `should solve a simple problem`() {
+        val problem = Problem(numberOfPlayers = 6, numberOfRounds = 8, playersPerMatch = 4)
+        val problemSolver = ProblemSolver(problem, parallelSolvers = 1)
+        val result = problemSolver.solve()
+
+        val schedule = result.schedule
+
+        assertThat(schedule.playersPerMatch).isEqualTo(4)
+        assertThat(schedule.numberOfRounds).isEqualTo(8)
+        assertThat(schedule.playersPerMatch).isEqualTo(4)
+
+        val score = result.score
+
+        assertThat(score).`as` { "Score should not be too bad." }.isLessThan(10.0)
+    }
+}
